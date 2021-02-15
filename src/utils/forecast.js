343e2eafbc1728106da715f2af9e3cf9969756c3
Chad Leonard <chad.leonard@burgessgroup.com>
2021-02-15 09:19:14 -0500

    added wind speed and direction to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -21,7 +21,12 @@ const forecast = (latitude, longitude, callback) => {
             callback(undefined, response.body.current.weather_descriptions[0] 
                 + ". It is currently " 
                 + response.body.current.temperature 
-                + " degrees out.")
+                + " degrees out."
+                + " It feels like it's "
+                + response.body.current.feelslike
+                + " degrees outside."
+                + " Wind speed and direction are "
+                + response.body.current.wind_speed + " mph and direction: " + response.body.current.wind_dir + " .")
         }
     })
 }
